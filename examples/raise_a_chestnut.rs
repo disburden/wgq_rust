8afd631c189cdc8831d27fecab840e31eb0cd85c
disburden <disburden@gmail.com>
2025-03-21 00:21:12 +0800

    增加parse_datetime方法,将&str字符串转换为DateTime<Utc>日期

--- examples/raise_a_chestnut.rs
+++ examples/raise_a_chestnut.rs
@@ -9,6 +9,7 @@ fn main(){
    test_rsa();
    test_bcrypt();
    test_aes();
+   test_data_type();
 
    println!("生成正常格式的uuid:{:?}",wgq_rust::obtain_uuid(wgq_rust::UuidFormat::Normal));
    println!("生成没有下划线的uuid:{:?}",wgq_rust::obtain_uuid(wgq_rust::UuidFormat::NoUnderline));
@@ -77,4 +78,25 @@ fn test_aes(){
    let cipher_bytes = wgq_rust::decrypt_use_base64(&cipher_text).unwrap();
    let plain_text = wgq_rust::decrypt_use_aes(key, iv, &cipher_bytes);
    println!("aes解密后的消息:{:?}",plain_text);
+}
+
+fn test_data_type(){
+   let date_str_with_z_rfc3339 = "2025-03-16T23:55:36.000Z";
+   let date_str_with_z_custom = "2025-03-16 23:55:36.000Z";
+   let date_str_without_z = "2025-03-16 23:55:36.000";
+
+   match wgq_rust::data_type::parse_datetime(date_str_with_z_rfc3339) {
+       Ok(dt) => println!("Parsed with Z (RFC 3339): {}", dt),
+       Err(e) => println!("Failed to parse: {}", e),
+   }
+
+   match wgq_rust::data_type::parse_datetime(date_str_with_z_custom) {
+       Ok(dt) => println!("Parsed with Z (custom): {}", dt),
+       Err(e) => println!("Failed to parse: {}", e),
+   }
+
+   match wgq_rust::data_type::parse_datetime(date_str_without_z) {
+       Ok(dt) => println!("Parsed without Z: {}", dt),
+       Err(e) => println!("Failed to parse: {}", e),
+   }
 }
--- src/data_type.rs
+++ src/data_type.rs
@@ -1,4 +1,7 @@
 use std::any::type_name;
+use chrono::{DateTime, FixedOffset, NaiveDateTime, Utc};
+use chrono::format::ParseError;
+use openssl::asn1::Asn1Type;
 
 /// 打印变量类型
 pub fn print_type_of<T>(_: &T) -> &'static str {
@@ -18,4 +21,31 @@ pub fn print_type_of_with_name<T>(_: &T, var_name: &str) -> &'static str {
 /// 将Vec<u8>转String
 pub fn vec_to_string(vec: Vec<u8>) -> String {
     String::from_utf8_lossy(&vec).to_string()
+}
+
+
+/// 将字符串转换为日期
+/// date_str: 带时区的日期字符串，包括
+/// 符合RFC 3339 格式的日期字符串
+/// "2025-03-16T23:55:36.000"
+/// "2025-03-16 23:55:36.000Z"
+/// 返回值: Result<DateTime<Utc>, ParseError>
+pub fn parse_datetime(date_str: &str) -> Result<DateTime<Utc>, anyhow::Error> {
+    // 尝试解析带时区的日期字符串（RFC 3339 格式）
+    if let Ok(dt) = DateTime::parse_from_rfc3339(date_str) {
+        return Ok(dt.with_timezone(&Utc));
+    }
+
+    // 尝试解析带时区的日期字符串（自定义格式，例如 "2025-03-16 23:55:36.000Z"）
+    if let Ok(dt) = DateTime::parse_from_str(date_str, "%Y-%m-%d %H:%M:%S%.fZ") {
+        return Ok(dt.with_timezone(&Utc));
+    }
+
+    // 尝试解析不带时区的日期字符串（假设为 UTC 时间）
+    if let Ok(ndt) = NaiveDateTime::parse_from_str(date_str, "%Y-%m-%d %H:%M:%S%.f") {
+        return Ok(DateTime::<Utc>::from_naive_utc_and_offset(ndt, Utc));
+    }
+
+    // 如果都不匹配，返回错误
+    anyhow::bail!("无法解析日期字符串");
 }
